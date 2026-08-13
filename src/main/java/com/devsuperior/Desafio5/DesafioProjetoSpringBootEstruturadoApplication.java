19a96cef4af2f96fdf9af860b1182d0f110a7dc7
Allex <1allex.moura@gmail.com>
2026-08-13 19:20:52 -0300

    Primeiro commit

---
+++ src/main/java/com/devsuperior/Desafio5/DesafioProjetoSpringBootEstruturadoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Desafio5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioProjetoSpringBootEstruturadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioProjetoSpringBootEstruturadoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/devsuperior/Desafio5/entities/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.devsuperior.Desafio5.entities;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+}
