a56bab20be4868aacd970da10c055ce048df4ed6
Allex <1allex.moura@gmail.com>
2026-08-13 21:05:22 -0300

    ProductMinDTO para retornar produtos sem descrição na busca por todos os produtos

--- src/main/java/com/devsuperior/Desafio5/controllers/ProductController.java
+++ src/main/java/com/devsuperior/Desafio5/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package com.devsuperior.Desafio5.controllers;
 
 import com.devsuperior.Desafio5.dto.ProductDTO;
+import com.devsuperior.Desafio5.dto.ProductMinDTO;
 import com.devsuperior.Desafio5.services.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +28,10 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(
+    public ResponseEntity<Page<ProductMinDTO>> findAll(
             @RequestParam(defaultValue = "") String name,
             Pageable pageable) {
-        Page<ProductDTO> dto = service.findAll(name, pageable);
+        Page<ProductMinDTO> dto = service.findAll(name, pageable);
         return ResponseEntity.ok(dto);
     }
 
